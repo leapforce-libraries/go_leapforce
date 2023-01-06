1838145635679786ae31c2c61ebb628a6a3e2af0
caselongo <caselongo@gmail.com>
2023-01-06 16:52:47 +0100

    Update hubspot.go

--- tools/hubspot.go
+++ tools/hubspot.go
@@ -28,3 +28,34 @@ func ValidatePhoneEncrypt(portalId string, objectType string, objectId string, p
 
 	return base64.StdEncoding.EncodeToString([]byte(encrypted)), nil
 }
+
+func GetKvkInfoEncrypt(portalId string, objectType string, objectId string, name *string, zip *string, address *string, country *string, cipherKey string) (string, *errortools.Error) {
+	var values = make(map[string]string)
+	values["portal_id"] = portalId
+	values["object_type"] = objectType
+	values["object_id"] = objectId
+	if name != nil {
+		values["company-name"] = *name
+	}
+	if zip != nil {
+		values["zip"] = *zip
+	}
+	if address != nil {
+		values["street"] = *address
+	}
+	if country != nil {
+		values["country"] = *country
+	}
+
+	b, err := json.Marshal(values)
+	if err != nil {
+		return "", errortools.ErrorMessage(err)
+	}
+
+	encrypted, err := utils.Encrypt(b, cipherKey)
+	if err != nil {
+		return "", errortools.ErrorMessage(err)
+	}
+
+	return base64.StdEncoding.EncodeToString([]byte(encrypted)), nil
+}
