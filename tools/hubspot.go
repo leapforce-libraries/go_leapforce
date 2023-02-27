ca97c7e0cc935bf3ffdf0ad4c0bb52f9d5aac4f6
caselongo <caselongo@gmail.com>
2023-02-27 10:49:14 +0100

    x

--- tools/hubspot.go
+++ tools/hubspot.go
@@ -7,14 +7,13 @@ import (
 	utilities "github.com/leapforce-libraries/go_utilities"
 )
 
-func ValidatePhoneEncrypt(portalId string, objectType string, objectId string, phoneNumber string, region string, field string, targetField string, cipherKey string) (string, *errortools.Error) {
+func ValidatePhoneEncrypt(portalId string, objectType string, objectId string, phoneNumber string, region string, targetField string, cipherKey string) (string, *errortools.Error) {
 	var values = make(map[string]string)
 	values["portal_id"] = portalId
 	values["object_type"] = objectType
 	values["object_id"] = objectId
 	values["phone"] = phoneNumber
 	values["region"] = region
-	values["field"] = field
 	values["target_field"] = targetField
 
 	b, err := json.Marshal(values)
